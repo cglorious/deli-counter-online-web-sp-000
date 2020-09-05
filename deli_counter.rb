459975a6873b0919cf49424a8f1ac8306e31b0ee
Catherine Gloria <catherinegloria@gmail.com>
2020-09-05 15:56:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(array)
   if array.length > 0
     #array.map do |element| #map returns new array
-      puts "Currently serving #{element}."
+      puts "Currently serving #{array[0]}."
       #array.drop #- how to take off first element and continue to iterate
       array.shift
       #array
